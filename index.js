4d87541236bf4d4d6b726900779ef5552de10115
sintti <erkka.karjalainen@gmail.com>
2020-09-08 09:59:42 +0300

    puhelinluettelon backend

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+// const requestLogger = (req, res, next) => {
+//     console.log('Method: ', req.method)
+//     console.log('Path: ', req.path)
+//     console.log('Body: ', req.body)
+//     console.log('--------')
+//     next()
+// }
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint'})
+}
+
+
 app.use(express.json())
+// app.use(requestLogger)
+app.use(morgan('tiny'))
+app.use(cors())
+
 
 let persons = [
     {
@@ -37,7 +56,60 @@ app.get('/info', (req, res) => {
     res.status(200).send(`<div><p>${info}</p><p>${new Date}</p></div>`)
 })
 
-const PORT = 3001
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    
+    
+    res.status(204).end()
+})
+
+const generateID = () => {
+    return Math.floor(Math.random() * Math.floor(1000))
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    } else if(!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.find(p => p.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID(),
+    }
+    
+    persons = persons.concat(person)
+    
+    res.json(person)
+})
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
