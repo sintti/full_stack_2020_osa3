ac84bb65292d16a703e6d9580a55a9f9ee128c26
sintti <erkka.karjalainen@gmail.com>
2020-09-09 00:08:10 +0300

    Tehtävät 3.13-3.14 puhelinluettelo ja tietokanta

--- index.js
+++ index.js
@@ -1,116 +1,75 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-
-// const requestLogger = (req, res, next) => {
-//     console.log('Method: ', req.method)
-//     console.log('Path: ', req.path)
-//     console.log('Body: ', req.body)
-//     console.log('--------')
-//     next()
-// }
+const Person = require('./modules/person')
+const { response } = require('express')
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'unknown endpoint'})
 }
 
-
 app.use(express.json())
 app.use(express.static('build'))
-// app.use(requestLogger)
 app.use(morgan('tiny'))
 app.use(cors())
 
-
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-532562546",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-423694116",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-123456789",
-        id: 4
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({})
+        .then(persons => {
+            res.json(persons)
+        })
 })
 
-app.get('/info', (req, res) => {
-    const length = persons.length
-    const info = `Phonebook has info for ${length}`
+// app.get('/info', (req, res) => {
+//     const length = persons.length
+//     const info = `Phonebook has info for ${length}`
     
-    res.status(200).send(`<div><p>${info}</p><p>${new Date}</p></div>`)
-})
+//     res.status(200).send(`<div><p>${info}</p><p>${new Date}</p></div>`)
+// })
 
+// Jatketaanpa tästä levänneillä aivoilla! :)
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id)
+        .then(person => {
+            res.json(person)
+        })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    
-    
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id)
+        .then(response => {
+            res.status(204).end()
+        })
 })
-
-const generateID = () => {
-    return Math.floor(Math.random() * Math.floor(1000))
-}
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
     
-    if (!body.name) {
+    if (body.name === undefined) {
         return res.status(400).json({
             error: 'name missing'
         })
-    } else if(!body.number) {
+    } else if(body.number === undefined) {
         return res.status(400).json({
             error: 'number missing'
         })
-    } else if (persons.find(p => p.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    } 
     
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateID(),
-    }
+    })
     
-    persons = persons.concat(person)
-    
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
